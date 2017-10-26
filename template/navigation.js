8e6e2b6538ced04203873955c30f6ee43f989cac
kristine919717 <kristine919717@gmail.com>
2017-10-26 16:54:03 -0700

    add search page add menu dropdown add one products page change some white space and font size

---
+++ template/navigation.js
@@ -0,0 +1,36 @@
+function openNav(){
+  document.getElementById('mobile-nav').style.height ="100%";
+}
+
+function closeNav(){
+  document.getElementById('mobile-nav').style.height ="0%";
+}
+
+function openSearch(){
+  document.getElementById('searchpage').style.height ="100%";
+}
+
+function closeSearch(){
+  document.getElementById('searchpage').style.height ="0%";
+}
+
+function gotocandle(){
+
+  window.location.href = 'products.html';
+  document.getElementById('searchpage').style.height ="0%";
+}
+
+function gotocontact(){
+
+  window.location.href = 'contact.html';
+  document.getElementById('searchpage').style.height ="0%";
+}
+function gotoreturn(){
+
+  window.location.href = 'return_policy.html';
+  document.getElementById('searchpage').style.height ="0%";
+}
+
+function goBack() {
+    window.history.back();
+}
